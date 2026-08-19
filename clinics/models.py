476c84f2181e23dc00a69dbe09bea3d0fe193c97
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-19 11:40:05 +0100

    model clinic created

--- clinics/models.py
+++ clinics/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Clinic(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    address = models.TextField(blank=True)
+    logo = models.ImageField(upload_to="clinics/logos/", blank=True, null=True)
+    document_header = models.TextField(blank=True) 
+    document_footer = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
