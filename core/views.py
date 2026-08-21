af07b18835b2ef734f5720ca73b58dc6957231e7
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-21 21:26:43 +0100

    updated the dashboard view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from clinics.profiles import DoctorProfile, SecretaryProfile
+
 # Create your views here.
 
 @login_required
 def dashboard(request):
-    return render(request, 'core/dashboard.html', )
+
+    clinic = request.user.clinic
+
+    doctors_count = DoctorProfile.objects.filter(clinic=clinic).count()
+
+    secretaries_count = SecretaryProfile.objects.filter(clinic=clinic).count()
+
+    context = {
+        "clinic": clinic,
+        "doctors_count": doctors_count,
+        "secretaries_count": secretaries_count
+    }
+
+    return render(request, 'core/dashboard.html', context )
