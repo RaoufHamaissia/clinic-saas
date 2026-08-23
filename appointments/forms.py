670493f17b842ed261898c1a3f3db6dc922285a6
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-23 20:23:13 +0100

    WalkIn appoitment form is created

--- appointments/forms.py
+++ appointments/forms.py
@@ -33,3 +33,24 @@ class AppointmentForm(forms.Form):
         if clinic is not None:
             self.fields["patient"].queryset = Patient.objects.for_clinic(clinic)  #type:ignore
             self.fields["doctor"].queryset = DoctorProfile.objects.filter(clinic=clinic)  #type:ignore
+
+
+class WalkInForm(forms.Form):
+    patient = forms.ModelChoiceField(
+        queryset=Patient.objects.none(),
+        label="Patient",
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
+
+    doctor = forms.ModelChoiceField(
+        queryset=DoctorProfile.objects.none(),
+        label="Doctor",
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
+
+    def __init__(self, *args, clinic=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if clinic is not None:
+            self.fields["patient"].queryset = Patient.objects.for_clinic(clinic) #type:ignore
+            self.fields["doctor"].queryset = DoctorProfile.objects.filter(clinic=clinic) #type:ignore
