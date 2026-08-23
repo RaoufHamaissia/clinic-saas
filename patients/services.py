f1f511d259eb2337646357d089f5955ee4568987
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-23 01:20:51 +0100

    patient service created

--- patients/services.py
+++ patients/services.py
@@ -10,3 +10,13 @@ class PatientService:
     @staticmethod
     def get_patient(clinic, patient_id):
         return Patient.objects.for_clinic(clinic).get(pk=patient_id) #type:ignore
+
+    @staticmethod
+    def create_patient(*, clinic, first_name, last_name, date_of_birth=None,
+                       phone="", address="", reason_for_visit=""):
+        return Patient.objects.create(clinic=clinic, first_name=first_name,
+                                      last_name=last_name, date_of_birth=date_of_birth,
+                                      phone=phone, address=address,
+                                      reason_for_visit=reason_for_visit)
+
+    
