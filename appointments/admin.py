0c98d99b84f38239f0c34eeaa592be3bf0a186a5
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-23 23:50:16 +0100

    model appointments registered in admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import Appointment
+
 # Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient",
+        "doctor",
+        "clinic",
+        "scheduled_at",
+        "status",
+        "is_walk_in",
+    )
+
+    list_filter = (
+        "clinic",
+        "status",
+        "is_walk_in",
+        "doctor",
+    )
+
+    search_fields = (
+        "patient__first_name",
+        "patient__last_name",
+        "doctor__user__email"
+    )
