fc5ad35b30451a1b27643ce9be3309f0bfd3d63e
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-23 10:39:21 +0100

    patients urls updated

---
+++ patients/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "patients"
+
+urlpatterns = [ 
+    path("", views.patient_list, name="list"),
+    path("add/", views.add_patient, name="add"),
+] 
