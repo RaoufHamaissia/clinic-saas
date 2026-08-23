49427eead246e051546009a739679aedac3ff683
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-23 10:32:20 +0100

    patient list view added

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .forms import PatientForm
+from .services import PatientService
+
 
 # Create your views here.
+
+@login_required
+def patient_list(request):
+    clinic = request.user.clinic
+
+    patients = PatientService.get_for_clinic(clinic)
+
+    context = {"patients": patients}
+    return render(request, "patients/list.html", context)
