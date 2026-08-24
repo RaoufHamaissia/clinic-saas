f368bfaec7381e46ff33b541254dedfc525937d3
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-24 22:13:57 +0100

    prescriptions urls done

--- records/urls.py
+++ records/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "records"
 
 urlpatterns = [
-    
+    path("prescriptions/", views.prescription_list, name="prescription_list"),
+    path("prescriptions/add/", views.add_prescription, name="prescription_add"),
+    path("prescriptions/<int:pk>/print/", views.prescription_print, name="prescription_print"),
 ]
