1d3902b47a99b71c8ff0fd071e35bf395fea76d7
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-24 22:15:25 +0100

    records models register in admin

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import DoctorDocumentProfile, Prescription, PrescriptionItem
+
 
 # Register your models here.
+
+class PrescriptionItemInline(admin.TabularInline):
+    model = PrescriptionItem
+    extra = 1
+
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ("patient", "doctor", "clinic", "created_at")
+    list_filter = ("clinic", "doctor")
+    search_fields = ("patient__first_name", "patient__last_name")
+    inlines = [PrescriptionItemInline]
+
+
+@admin.register(DoctorDocumentProfile)
+class DoctorDocumentProfileAdmin(admin.ModelAdmin):
+    list_display = ("doctor", "professional_title", "registration_number")
