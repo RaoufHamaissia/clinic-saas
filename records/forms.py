dd0168fb102501b3ea94c9e4535b87eae6f7a4d5
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-25 14:47:46 +0100

    DoctorNoteForm

--- records/forms.py
+++ records/forms.py
@@ -60,3 +60,29 @@ class PrescriptionItemForm(forms.Form):
         )
 
 PrescriptionItemFormSet = formset_factory(PrescriptionItemForm, extra=3, can_delete=True)
+
+
+class DoctorNoteForm(forms.Form):
+    patient = forms.ModelChoiceField(
+        queryset=Patient.objects.none(),
+        label="Patient",
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
+
+    doctor = forms.ModelChoiceField(
+        queryset=DoctorProfile.objects.none(),
+        label="Doctor",
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
+
+    content = forms.CharField(
+        label="Note",
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": 6})
+    )
+
+    def __init__(self, *args, clinic=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if clinic is not None:
+            self.fields["patient"].queryset = Patient.objects.for_clinic(clinic) #type:ignore
+            self.fields["doctor"].queryset = DoctorProfile.objects.filter(clinic=clinic) #type:ignore
