5795b8bbe4fc52405c2bf8419e6baee942e2f673
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-25 11:32:45 +0100

    medication suggest url

--- records/urls.py
+++ records/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("prescriptions/", views.prescription_list, name="prescription_list"),
     path("prescriptions/add/", views.add_prescription, name="prescription_add"),
     path("prescriptions/<int:pk>/print/", views.prescription_print, name="prescription_print"),
+    path("medications/suggest/", views.medication_suggest, name="medication_suggest"),
 ]
