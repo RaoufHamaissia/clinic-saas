2dc947ba2489ca280e0dfb7ae0025e743b6a2379
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-26 12:55:37 +0100

    Staff managment urls created

--- clinics/urls.py
+++ clinics/urls.py
@@ -6,4 +6,10 @@ app_name = "clinics"
 urlpatterns = [
     path("register/", views.register_clinic, name="register"),
     path("specialties/suggest/", views.specialty_suggest, name="specialty_suggest"),
+
+    path("doctors/", views.doctor_list, name="doctor_list"),
+    path("doctors/add/", views.add_doctor, name="doctor_add"),
+    
+    path("secretaries/", views.secretary_list, name="secretary_list"),
+    path("secretaries/add/", views.add_secretary, name="secretary_add"),
 ]
