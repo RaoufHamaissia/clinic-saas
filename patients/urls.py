b55f83727e72e50bf2158837fdd7e7ffefb678df
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-28 21:23:06 +0100

    updated  patients urls

--- patients/urls.py
+++ patients/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("", views.patient_list, name="list"),
     path("add/", views.add_patient, name="add"),
     path("<int:pk>/", views.patient_detail, name="detail"),
+    path("<int:patient_id>/prescriptions/add/", views.add_prescription, name="add_prescription"),
+    path("<int:patient_id>/notes/add/", views.add_note, name="add_note"),
+    path("<int:patient_id>/procedure-reports/add/", views.add_procedure_report, name="add_procedure_report"),
+    path("<int:patient_id>/labwork-demands/add/", views.add_labwork_demand, name="add_labwork_demand"),
+
 ] 
