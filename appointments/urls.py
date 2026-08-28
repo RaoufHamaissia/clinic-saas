2907d30c42df4b28f58fa8d3f88220463c2be97e
raouf hamaissia <r.hamaissia@gmail.com>
2026-08-28 13:02:32 +0100

    updated appointment urls

--- appointments/urls.py
+++ appointments/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("add/", views.add_appointment, name="add"),    
     path("walk-in/", views.add_walk_in, name="walk_in"),    
     path("<int:pk>/status/", views.update_status, name="update_status"),    
+    path("types/suggest/", views.appointment_type_suggest, name="type_suggest"),
     path("<str:date>/", views.day_view, name="day_for_date"),    
 ]
