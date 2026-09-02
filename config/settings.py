b8e95c0b805c3077a711d904624ebefad6f5a13c
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-02 12:11:55 +0100

    setting up Field encryption key

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+FIELD_ENCRYPTION_KEY = env('FIELD_ENCRYPTION_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
