cb095cb7d2ae9bbb23fe41277dbbb90e7aa4ca2f
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-02 20:13:35 +0100

    log view url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard
+from .views import audit_log_view, dashboard
 
 
 app_name = "core"
 
 urlpatterns = [
-    path("", dashboard, name="dashboard")
+    path("", dashboard, name="dashboard"),
+    path("audit-log/", audit_log_view, name="audit_log"),
 ]
