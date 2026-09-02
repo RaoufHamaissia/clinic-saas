3f9c1e8caf89541fd5ca78d54261982b0dee43ad
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-02 19:55:40 +0100

    added current user contextVar

--- core/context.py
+++ core/context.py
@@ -1,6 +1,8 @@
 from contextvars import ContextVar
 
 _current_clinic: ContextVar = ContextVar("current_clinic", default=None)
+_current_user: ContextVar = ContextVar("current_user", default=None)
+
 
 def set_current_clinic(clinic):
     _current_clinic.set(clinic)
@@ -10,3 +12,15 @@ def get_current_clinic():
 
 def clear_current_clinic():
     _current_clinic.set(None)
+
+
+def set_current_user(user):
+    _current_user.set(user)
+
+
+def get_current_user():
+    return _current_user.get()
+
+
+def clear_current_user():
+    _current_user.set(None)
