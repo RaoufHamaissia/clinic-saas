fc4bf8eed01fa4842bca36404ee87b7ae1b3f909
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-03 20:16:36 +0100

    billing models registered in admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Subscription, VisitRecord, Invoice
 # Register your models here.
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("clinic", "plan", "status", "trial_ends_at")
+    list_filter = ("plan", "status")
+
+
+@admin.register(VisitRecord)
+class VisitRecordAdmin(admin.ModelAdmin):
+    list_display = ("clinic", "appointment", "amount", "invoice", "created_at")
+    list_filter = ("clinic",)
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ("clinic", "plan", "period_start", "period_end", "amount_due", "status")
+    list_filter = ("plan", "status")
