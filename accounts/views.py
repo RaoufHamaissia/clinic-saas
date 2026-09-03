0728be6a147541f37290adfb210acb2b271d356e
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-03 21:11:15 +0100

    edit profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from django.contrib.auth.forms import PasswordChangeForm
+
+from .forms import LoginForm, ProfileForm
+
 
 # Create your views here.
 
@@ -54,3 +57,37 @@ def logout_view(request):
         return redirect("accounts:login")
     
     return redirect("core:dashboard")
+
+
+@login_required
+def edit_profile(request):
+    if request.method == "POST":
+        if "update_profile" in request.POST:
+            profile_form = ProfileForm(request.POST, instance=request.user)
+            password_form = PasswordChangeForm(request.user)
+
+            if profile_form.is_valid():
+                profile_form.save()
+                messages.success(request, "Profile updated successfully")
+                return redirect("accounts:edit_profile")
+
+        elif "change_password" in request.POST:
+            profile_form = ProfileForm(instance=request.user)
+            password_form = PasswordChangeForm(request.user, request.POST)
+
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)  # keeps the user logged in after password change
+                messages.success(request, "Password changed successfully")
+                return redirect("accounts:edit_profile")
+
+        else:
+            profile_form = ProfileForm(instance=request.user)
+            password_form = PasswordChangeForm(request.user)
+
+    else:
+        profile_form = ProfileForm(instance=request.user)
+        password_form = PasswordChangeForm(request.user)
+
+    context = {"profile_form": profile_form, "password_form": password_form}
+    return render(request, "accounts/edit_profile.html", context)
