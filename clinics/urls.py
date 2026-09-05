92b29f314bc06807874233ddf336e05edebadbde
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-05 15:15:23 +0100

    users detail pages +  profile activation/deactivation templates + urls

--- clinics/urls.py
+++ clinics/urls.py
@@ -9,9 +9,17 @@ urlpatterns = [
 
     path("doctors/", views.doctor_list, name="doctor_list"),
     path("doctors/add/", views.add_doctor, name="doctor_add"),
+
+    path("doctors/<int:pk>/", views.doctor_detail, name="doctor_detail"),
+    path("doctors/<int:pk>/toggle-active/", views.toggle_doctor_active, name="doctor_toggle_active"),
+    
     
     path("secretaries/", views.secretary_list, name="secretary_list"),
     path("secretaries/add/", views.add_secretary, name="secretary_add"),
+
+    path("secretaries/<int:pk>/", views.secretary_detail, name="secretary_detail"),
+    path("secretaries/<int:pk>/toggle-active/", views.toggle_secretary_active, name="secretary_toggle_active"),
+    
 
     path("settings/", views.clinic_settings, name="settings"),
 ]
