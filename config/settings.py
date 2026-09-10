7133afa0b19afa579a34451a6c04f43779f006b1
raouf hamaissia <r.hamaissia@gmail.com>
2026-09-10 14:24:13 +0100

    configured languages in settings

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+LANGUAGES = [
+    ('en', 'English'),
+    ('fr', 'Français'),
+]
+
+LOCAL_PATHS = [
+    BASE_DIR / 'locale',
+]
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
